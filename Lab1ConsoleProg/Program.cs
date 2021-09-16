9fe79973eb6cebe39c14fa342c26ff8f658f7030
Pavel <patel.starovoytov2003@gmail.com>
2021-09-16 23:10:51 +0300

    Test commit

--- Lab1ConsoleProg/Program.cs
+++ Lab1ConsoleProg/Program.cs
@@ -2,34 +2,68 @@
 using Lab1ConsoleProg.ProgramAnalyzer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab1ConsoleProg
 {
 	class Program
 	{
-		
+		class OperandsAndOperators
+		{
+			public string Value { get; set; }
+			public int NumOfRep { get; set; }
+
+		}
+
 		static void Main(string[] args)
 		{
 			List<ProgramEntity> programEntities = new List<ProgramEntity>(){
 				new ProgramEntity(ProgramType.Operand,"for"),
 				new ProgramEntity(ProgramType.Operator,"x"),
-				new ProgramEntity(ProgramType.Operand,"while")
+				new ProgramEntity(ProgramType.Operand,"while"),
+				new ProgramEntity(ProgramType.Operand,"while"),
+				new ProgramEntity(ProgramType.Operand,"for"),
+				new ProgramEntity(ProgramType.Operator,"x")
 			};
-			programEntities.RemoveAt(0);
-			foreach (var i in programEntities)
+
+			List<OperandsAndOperators> ListOfOperands = new List<OperandsAndOperators>();
+			List<OperandsAndOperators> ListOfOperators = new List<OperandsAndOperators>();
+
+			var ExpressionGroups = from ProgramEntity in programEntities
+							   group ProgramEntity by ProgramEntity.Value into g
+							   select new
+							   {
+								   Value = g.Key,
+								   Count = g.Count(),
+								   programEntities = from p in g select p
+							   };
+
+
+			foreach (var group in ExpressionGroups)
 			{
-				Console.WriteLine(i.Type + " : " + i.Value);
+				foreach (ProgramEntity ProgramEntity in group.programEntities)
+                {
+					if (ProgramType.Operand == ProgramEntity.Type)
+					{
+						ListOfOperands.Add(new OperandsAndOperators() { Value = group.Value, NumOfRep = group.Count });
+					} else
+                    {
+						ListOfOperators.Add(new OperandsAndOperators() { Value = group.Value, NumOfRep = group.Count });
+					}
+					break;
+                }
 			}
-			int[] array = { 10, 5, 10, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
-			int[] counts = new int[20 + 1];
-			for (int i = 0; i < array.Length; i++)
-				if (counts[array[i]] == 0)
-				{
-					for (int j = 0; j < array.Length; j++)
-						if (array[i] == array[j])
-							counts[array[i]]++;
-					Console.WriteLine(array[i] + " повторяется " + counts[array[i]] + " раз");
-				}
+
+			foreach (OperandsAndOperators p in ListOfOperands)
+			{
+				Console.WriteLine($"{p.Value} {p.NumOfRep}");
+			}
+			Console.WriteLine();
+			foreach (OperandsAndOperators p in ListOfOperators)
+			{
+				Console.WriteLine($"{p.Value} {p.NumOfRep}");
+			}
+			Console.ReadKey();
 		}
 	}
 }
