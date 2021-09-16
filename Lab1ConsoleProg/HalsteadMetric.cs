3646e455d9945e3d7eef6e147ea03b9c13516d41
Pavel <patel.starovoytov2003@gmail.com>
2021-09-16 23:10:51 +0300

    Test commit

--- Lab1ConsoleProg/HalsteadMetric.cs
+++ Lab1ConsoleProg/HalsteadMetric.cs
@@ -1,6 +1,7 @@
 ﻿using Lab1ConsoleProg.Enties;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Lab1ConsoleProg
@@ -9,10 +10,49 @@ namespace Lab1ConsoleProg
 	{
 		private readonly List<ProgramEntity> programEntities;
 
+		public List<OperandsAndOperators> ListOfOperands = new List<OperandsAndOperators>();
+		public List<OperandsAndOperators> ListOfOperators = new List<OperandsAndOperators>();
+
 		public HalsteadMetric(IEnumerable<ProgramEntity> programEntities)
 		{
 			this.programEntities = (List<ProgramEntity>)programEntities;
 		}
+
+		public void CalculateMetric()
+		{	
+			var ExpressionGroups = from ProgramEntity in programEntities
+								   group ProgramEntity by ProgramEntity.Value into g
+								   select new
+								   {
+									   Value = g.Key,
+									   Count = g.Count(),
+									   programEntities = from p in g select p
+								   };
+
+
+			foreach (var group in ExpressionGroups)
+			{
+				foreach (ProgramEntity ProgramEntity in group.programEntities)
+				{
+					if (ProgramType.Operand == ProgramEntity.Type)
+					{
+						ListOfOperands.Add(new OperandsAndOperators() { Value = group.Value, NumOfRep = group.Count });
+					}
+					else
+					{
+						ListOfOperators.Add(new OperandsAndOperators() { Value = group.Value, NumOfRep = group.Count });
+					}
+					break;
+				}
+			}
+		}
+
+	}
+
+	class OperandsAndOperators
+	{
+		public string Value { get; set; }
+		public int NumOfRep { get; set; }
 
 	}
 }
--- Lab1ConsoleProg/Program.cs
+++ Lab1ConsoleProg/Program.cs
@@ -2,34 +2,37 @@
 using Lab1ConsoleProg.ProgramAnalyzer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab1ConsoleProg
 {
 	class Program
 	{
-		
 		static void Main(string[] args)
 		{
 			List<ProgramEntity> programEntities = new List<ProgramEntity>(){
 				new ProgramEntity(ProgramType.Operand,"for"),
 				new ProgramEntity(ProgramType.Operator,"x"),
-				new ProgramEntity(ProgramType.Operand,"while")
+				new ProgramEntity(ProgramType.Operand,"while"),
+				new ProgramEntity(ProgramType.Operand,"while"),
+				new ProgramEntity(ProgramType.Operand,"for"),
+				new ProgramEntity(ProgramType.Operator,"x")
 			};
-			programEntities.RemoveAt(0);
-			foreach (var i in programEntities)
+
+			HalsteadMetric metric = new HalsteadMetric(programEntities);
+			metric.CalculateMetric();
+
+			foreach (OperandsAndOperators p in metric.ListOfOperands)
 			{
-				Console.WriteLine(i.Type + " : " + i.Value);
+				Console.WriteLine($"{p.Value} {p.NumOfRep}");
 			}
-			int[] array = { 10, 5, 10, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
-			int[] counts = new int[20 + 1];
-			for (int i = 0; i < array.Length; i++)
-				if (counts[array[i]] == 0)
-				{
-					for (int j = 0; j < array.Length; j++)
-						if (array[i] == array[j])
-							counts[array[i]]++;
-					Console.WriteLine(array[i] + " повторяется " + counts[array[i]] + " раз");
-				}
+			Console.WriteLine();
+			foreach (OperandsAndOperators p in metric.ListOfOperators)
+			{
+				Console.WriteLine($"{p.Value} {p.NumOfRep}");
+			}
+			Console.ReadKey();
+
 		}
 	}
 }
