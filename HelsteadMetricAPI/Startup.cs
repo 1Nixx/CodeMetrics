4bbb6aa605bb550c9602b512f77ca86b53f883dd
Nikita H <hripach.nik.29@gmail.com>
2021-09-25 15:33:51 +0300

    Add Swagger

--- HelsteadMetricAPI/Startup.cs
+++ HelsteadMetricAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,7 @@ namespace HelsteadMetricAPI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+			services.AddSwaggerGen();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +39,9 @@ namespace HelsteadMetricAPI
 			}
 
 			app.UseHttpsRedirection();
+
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
 			app.UseRouting();
 
