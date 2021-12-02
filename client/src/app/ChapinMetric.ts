0e48aa6d652eddd26662989962b8993b6ebeb1d3
Nikita H <hripach.nik.29@gmail.com>
2021-12-02 15:05:24 +0300

    Update Front chepin type

--- client/src/app/ChapinMetric.ts
+++ client/src/app/ChapinMetric.ts
@@ -1,18 +1,24 @@
-export interface ChapinTypes {
+import * as internal from "stream";
+
+export interface ChapinTypesVariable {
   P: string[];
   M: string[];
   C: string[];
   T: string[];
 }
 
-export interface ChapinIOTypes {
-  P: any[];
-  M: any[];
-  C: string[];
-  T: any[];
+export interface ChapinTypesCount {
+  P: Number;
+  M: Number;
+  C: Number;
+  T: Number;
 }
 
 export interface IChapinMetric {
-  chapinTypes: ChapinTypes;
-  chapinIOTypes: ChapinIOTypes;
+  chapinTypes: ChapinTypesVariable;
+  variableCount:  ChapinTypesCount;
+  chapinIOTypes: ChapinTypesVariable;
+  variableIOCount: ChapinTypesCount;
+  metricResult: Number;
+  metricIOResult: Number;
 }
